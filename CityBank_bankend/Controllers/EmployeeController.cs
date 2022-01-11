c7d5a15ce3e5519d5ade8dc41f0e92118f8bd8c6
hothayfa <hothifa19982020@gmail.com>
2022-01-11 16:26:04 +0200

    first commit

---
+++ CityBank_bankend/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using common;
+using DAL;
+namespace CityBank_bankend.Controllers
+{
+    [ApiController]
+    public class EmployeeController
+    {
+        #region Api for Create Employee
+        [HttpPost]
+        [Route("/api/employee/create")]
+        public Guid CreateEmployee(Employees employee)
+        {
+            EmployeeRepository er = new EmployeeRepository();
+            var result=er.CreateAccount(employee);
+            if (result == null)
+            {
+                return new Guid();
+            }
+            else
+            {
+                return result;
+            }
+        }
+        #endregion
+
+        #region Api for Login
+
+        #endregion
+    }
+}
